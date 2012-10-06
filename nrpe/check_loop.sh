c29d68c0f482d62953eb398505bfb819a7a8f111
Olivier Renault <monoliv@gmail.com>
2012-10-06 11:29:16 +0100

    Add few scripts to add as plugin on clients

---
+++ nrpe/check_loop.sh
@@ -0,0 +1,60 @@
+#! /bin/sh
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+
+PROGNAME=`basename $0`
+PROGPATH=`echo $0 | sed -e 's,[\\/][^\\/][^\\/]*$,,'`
+REVISION="1.0.0"
+
+. $PROGPATH/utils.sh
+
+
+print_usage() {
+	echo "Usage: $PROGNAME"
+}
+
+print_help() {
+	print_revision $PROGNAME $REVISION
+	echo ""
+	print_usage
+	echo ""
+	echo "This plugin checks loopback devices availability ."
+	echo ""
+	support
+	exit 0
+}
+
+case "$1" in
+	--help)
+		print_help
+		exit 0
+		;;
+	-h)
+		print_help
+		exit 0
+		;;
+	--version)
+   	print_revision $PROGNAME $REVISION
+		exit 0
+		;;
+	-V)
+		print_revision $PROGNAME $REVISION
+		exit 0
+		;;
+	*)
+                LOOPBACK_WARN=$1
+                LOOPBACK_CRIT=$2
+		LOOPBACK_USED=`losetup -a | wc -l`
+                LOOPBACK_AVAILABLE=`ls /dev/loop* | wc -l`
+		if test $(( ${LOOPBACK_AVAILABLE} - ${LOOPBACK_USED} )) -gt ${LOOPBACK_WARN} ; then
+			echo "LOOPBACK OK"
+			exit 0
+                elif [ $(( ${LOOPBACK_AVAILABLE} - ${LOOPBACK_USED} )) -lt ${LOOPBACK_WARN} ] && [ $(( ${LOOPBACK_AVAILABLE} - ${LOOPBACK_USED} )) -gt ${LOOPBACK_CRIT} ]; then
+			echo "WARNING - LOOPBACK AVAILABLE =  $(( ${LOOPBACK_AVAILABLE} - ${LOOPBACK_USED} ))"
+			exit 1
+		elif  test $(( ${LOOPBACK_AVAILABLE} - ${LOOPBACK_USED} )) -le ${LOOPBACK_CRIT} ; then
+			echo "SENSOR CRITICAL - LOOPBACK AVAILABLE < ${LOOPBACK_CRIT}" 
+			exit 2
+		fi
+		;;
+esac
