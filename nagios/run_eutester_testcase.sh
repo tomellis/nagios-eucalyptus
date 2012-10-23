9e586e2aed8141e70ffdece5bbbef42e51cdd696
Tom Ellis <tom.ellis@eucalyptus.com>
2012-10-23 18:58:23 -0400

    add script to call eutester testcases from cron

---
+++ nagios/run_eutester_testcase.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# 
+# This script runs Eutester's basic instance test testcase and 
+# outputs the result in /tmp/results for the nagios check script 
+# to parse. Call this script from cron every hour to test your
+# cloud.
+#
+# Authors: Olivier Renault <olivier.renault@eucalyptus.com>
+#          Tom Ellis <tom.ellis@eucalyptus.com>
+
+# Replace these variables as necessary
+CREDPATH='/root/.eucarc/'
+TEST='BasicInstanceChecks'
+EMI='emi-AE6A355B'
+PATH_TO_TESTCASE='/root/eutester/testcases/cloud_user/instances/instancetest.py'
+
+# Run the test
+$TESTCASE --credpath=$CREDPATH --tests $TEST --emi $EMI --nagios
